1b5220be8a9231f8119822955a0efc53d3e8fc22
Doug Latornell <dlatornell@eoas.ubc.ca>
2019-10-26 17:12:30 -0400

    Replace mock patches w/ pytest monkeypatch fixture in gather unit tests
    monkeypatch has a more explicit API, and is less magical than
    unittest.mock.patch.

--- tests/test_gather.py
+++ tests/test_gather.py
@@ -16,17 +16,16 @@
 """
 from pathlib import Path
 from types import SimpleNamespace
-from unittest.mock import Mock, patch
 
-import cliff.app
 import pytest
 
+import nemo_cmd.main
 import nemo_cmd.gather
 
 
 @pytest.fixture
 def gather_cmd():
-    return nemo_cmd.gather.Gather(Mock(spec=cliff.app.App), [])
+    return nemo_cmd.gather.Gather(nemo_cmd.main.NEMO_App, [])
 
 
 class TestGetParser:
@@ -36,6 +35,19 @@ class TestGetParser:
     def test_get_parser(self, gather_cmd):
         parser = gather_cmd.get_parser("nemo gather")
         assert parser.prog == "nemo gather"
+
+    def test_cmd_description(self, gather_cmd):
+        parser = gather_cmd.get_parser("nemo gather")
+        assert parser.description.strip().startswith(
+            "Gather the results files from the NEMO run in the present working"
+        )
+
+    def test_results_dir_argument(self, gather_cmd):
+        parser = gather_cmd.get_parser("nemo gather")
+        assert parser._actions[1].dest == "results_dir"
+        assert parser._actions[1].metavar == "RESULTS_DIR"
+        assert parser._actions[1].type == Path
+        assert parser._actions[1].help
 
     def test_parsed_args_defaults(self, gather_cmd):
         parser = gather_cmd.get_parser("nemo gather")
@@ -47,8 +59,16 @@ class TestTakeAction:
     """Unit test for `nemo gather` sub-command take_action() method.
     """
 
-    @patch("nemo_cmd.gather.gather", autospec=True)
-    def test_take_action(self, m_gather, gather_cmd):
-        parsed_args = SimpleNamespace(results_dir=Path("/results/"))
+    def test_take_action(self, gather_cmd, tmp_path, monkeypatch):
+        def mock_move_results(*args):
+            pass
+
+        def mock_delete_symlinks(*args):
+            pass
+
+        monkeypatch.setattr(nemo_cmd.gather, "_move_results", mock_move_results)
+        monkeypatch.setattr(nemo_cmd.gather, "_delete_symlinks", mock_delete_symlinks)
+        results_dir = tmp_path / "results_dir"
+        parsed_args = SimpleNamespace(results_dir=results_dir)
         gather_cmd.take_action(parsed_args)
-        m_gather.assert_called_once_with(Path("/results/"))
+        assert results_dir.exists()
