62fb940005a9ef3a98a4ded6b9384898b1bd40fe
Doug Latornell <djl@douglatornell.ca>
2019-12-09 12:32:45 -0800

    Replace mock patches w/ pytest monkeypatch fixture in deflate unit tests
    monkeypatch has a more explicit API, and is less magical than
    unittest.mock.patch.

--- tests/test_deflate.py
+++ tests/test_deflate.py
@@ -14,19 +14,21 @@
 # limitations under the License.
 """NEMO-Cmd deflate sub-command plug-in unit tests
 """
+import logging
+import math
+import multiprocessing
 from pathlib import Path
 from types import SimpleNamespace
-from unittest.mock import call, Mock, patch
 
-import cliff.app
 import pytest
 
+import nemo_cmd.main
 import nemo_cmd.deflate
 
 
 @pytest.fixture
 def deflate_cmd():
-    return nemo_cmd.deflate.Deflate(Mock(spec=cliff.app.App), [])
+    return nemo_cmd.deflate.Deflate(nemo_cmd.main.NEMO_App, [])
 
 
 class TestParser:
@@ -37,10 +39,39 @@ class TestParser:
         parser = deflate_cmd.get_parser("nemo deflate")
         assert parser.prog == "nemo deflate"
 
-    def test_parsed_args(self, deflate_cmd):
+    def test_cmd_description(self, deflate_cmd):
         parser = deflate_cmd.get_parser("nemo deflate")
-        parsed_args = parser.parse_args(["foo.nc", "bar.nc", "-j6"])
+        assert parser.description.strip().startswith(
+            "Deflate variables in netCDF files using Lempel-Ziv compression."
+        )
+
+    def test_filepaths_argument(self, deflate_cmd):
+        parser = deflate_cmd.get_parser("nemo deflate")
+        assert parser._actions[1].dest == "filepaths"
+        assert parser._actions[1].metavar == "FILEPATH"
+        assert parser._actions[1].type == Path
+        assert parser._actions[1].help
+
+    def test_jobs_option(self, deflate_cmd):
+        parser = deflate_cmd.get_parser("nemo deflate")
+        assert parser._actions[2].dest == "jobs"
+        assert parser._actions[2].type == int
+        assert parser._actions[2].default == math.floor(multiprocessing.cpu_count() / 2)
+        assert parser._actions[2].help
+
+    def test_parsed_arg(self, deflate_cmd):
+        parser = deflate_cmd.get_parser("nemo deflate")
+        parsed_args = parser.parse_args(["foo.nc", "bar.nc"])
         assert parsed_args.filepaths == [Path("foo.nc"), Path("bar.nc")]
+
+    def test_parsed_args_option_default(self, deflate_cmd):
+        parser = deflate_cmd.get_parser("nemo deflate")
+        parsed_args = parser.parse_args(["foo.nc"])
+        assert parsed_args.jobs == math.floor(multiprocessing.cpu_count() / 2)
+
+    def test_parsed_args_option_value(self, deflate_cmd):
+        parser = deflate_cmd.get_parser("nemo deflate")
+        parsed_args = parser.parse_args(["foo.nc", "-j6"])
         assert parsed_args.jobs == 6
 
 
@@ -48,10 +79,13 @@ class TestTakeAction:
     """Unit test for `nemo deflate` sub-command take_action() method.
     """
 
-    @patch("nemo_cmd.deflate.deflate", autospec=True)
-    def test_take_action(self, m_deflate, deflate_cmd):
+    def test_take_action(self, deflate_cmd, caplog, monkeypatch):
+
         parsed_args = SimpleNamespace(
             filepaths=[Path("foo.nc"), Path("bar.nc")], jobs=6
         )
+        caplog.set_level(logging.INFO)
         deflate_cmd.take_action(parsed_args)
-        m_deflate.assert_called_once_with([Path("foo.nc"), Path("bar.nc")], 6)
+        assert caplog.records[0].levelname == "INFO"
+        expected = "Deflating in up to 6 concurrent sub-processes"
+        assert caplog.messages[0] == expected
